11493c0d913ff9a0499c38f9e318584169eefea5
Kaushal Patel <patelkaushalb1@gmail.com>
2024-02-20 23:54:57 -0500

    Cleaned up code into functions and added full documentation.

--- src/ml.py
+++ src/ml.py
@@ -5,15 +5,29 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn import metrics
 import joblib
 
+### CONSTANTS ###
+
+# Windows Paths to the data files
 WINDOWS_PATH_1 = 'data\data_final.json'
 WINDOWS_PATH_2 = 'data\elliptic_txs_features.csv'
+
+# Linux Paths to the data files
 LINUX_PATH_1 = '../data/data_final.json'
 LINUX_PATH_2 = '../data/elliptic_txs_features.csv'
+
+# Model Save Path and Extension
 MODEL_SAVE_PATH_LINUX = '../model/'
 MODEL_EXTENSION = '.joblib'
 
+### FUNCTIONS ###
 
 def get_edges_dataframe(df):
+    """
+    Extracts the 'Edges' column from the DataFrame and returns a new DataFrame with the edges data.
+    :param df: The DataFrame to extract the edges from
+    :return: A new DataFrame with the edges data
+    """
+
     # Explode the "Edges" column to separate the dictionary into individual rows
     edges_df = df.explode('Edges').reset_index(drop=True)
 
@@ -37,25 +51,50 @@ def get_edges_dataframe(df):
 
 
 def load_json_file(file):
+    """
+    Loads a JSON file into a dictionary
+    :param file: The file to load
+    :return: A dictionary with the JSON data
+    """
+
     with open(file, 'r') as f:
         data = json.load(f)
 
     return data
 
 
+def print_divider():
+    """
+    Prints a divider to separate the output
+    :return: None
+    """
+
+    print("\n\n" + "=" * 100)
+    print("=" * 100 + "\n")
+
+
 def print_dataframe(df, header):
+    """
+    Prints the DataFrame with a header
+    :param df: The DataFrame to print
+    :param header: The header to print
+    :return: None
+    """
+
     print("\n\n" + "=" * 100)
     print(header)
     print("=" * 100 + "\n")
     print(df)
-
-
-def print_divider():
-    print("\n\n" + "=" * 100)
-    print("=" * 100 + "\n")
+    print_divider()
 
 
 def build_csv_dataframe(file):
+    """
+    Reads the CSV file into a DataFrame and returns it
+    :param file: The CSV file to read
+    :return: A DataFrame with the CSV data
+    """
+
     # Column Names for the CSV
     column_names = ['Node ID', 'Timestep'] + [f'Feature {i}' for i in range(2, 167)]
 
@@ -69,6 +108,12 @@ def build_csv_dataframe(file):
 
 
 def build_json_dataframe(file):
+    """
+    Builds a DataFrame from the JSON file and returns it
+    :param file: The JSON file to read
+    :return: A DataFrame with the JSON data
+    """
+
     # Open and load the JSON files into a dictionary
     data_final_dict = load_json_file(file)
 
@@ -96,75 +141,116 @@ def build_json_dataframe(file):
 
 
 def create_dataframe(file, is_feature):
+    """
+    Creates a DataFrame from the specified file. If the file is a JSON file, it will be converted into a DataFrame using the build_json_dataframe function. If the file is a CSV file, it will be read into a DataFrame using the build_csv_dataframe function.
+
+    :param file: The file to create the DataFrame from
+    :param is_feature: A boolean to indicate if the file is a feature file
+    :return: A DataFrame with the data from the file
+    """
+
     # Initialize the DataFrame
     df = None
 
     if not is_feature:
         # Convert the JSON data into a DataFrame
         df, edges_df = build_json_dataframe(file)
-
-        # Print the DataFrame
-        print_dataframe(df, "Data Final")
     else:
         # Read the CSV file into a DataFrame
         df = build_csv_dataframe(file)
-
-        # Print the DataFrame
-        print_dataframe(df, "Features")
-
-    # Print divider
-    print_divider()
 
     return df
 
 
 def merge_dataframes(data_final, features):
+    """
+    Merges the 'data_final' and 'features' DataFrames on 'Node ID' and returns the merged DataFrame
+    :param data_final: The 'data_final' DataFrame
+    :param features: The 'features' DataFrame
+    :return: The merged DataFrame
+    """
+
     # Merge the DataFrames on 'Node ID'
     df = pd.merge(data_final, features, on=['Node ID'], how='inner')
 
     # Rename the Timestep column to 'Feature 1' since it is the first feature from the original CSV
     df.rename(columns={'Timestep': 'Feature 1'}, inplace=True)
 
-    # Print the DataFrame
-    print_dataframe(df, "Combined Data")
-
-    # Print divider
-    print_divider()
-
     return df
 
+def separate_unknown(df):
+    """
+    Separates the DataFrame into two DataFrames. One with the instances where the group is 3 (unknown group) and another with the instances where the group is not 3 (Illicit or Licit group)
+    :param df: The DataFrame to separate
+    :return: Two DataFrames. One with the instances where the group is 3 (unknown group) and another with the instances where the group is not 3 (Illicit or Licit group)
+    """
 
-def machine_learning(df):
-    # Separate the instances where the group is 3
+    # Separate the instances where the group is 3 (unknown group)
     df_predict = df[df['Group'] == 3]
+
+    # Separate the instances where the group is not 3 (Illicit or Licit group)
     df_train_test = df[df['Group'] != 3]
 
-    # Print the DataFrames
-    print_dataframe(df_predict, "Predict Data")
-    print_dataframe(df_train_test, "Train and Test Data")
-    # Print the type of data for the group column
-    print(f"Group Data Type: {df_train_test['Group'].dtype}")
-    #exit(4)
+    # df_predict will be the transactions with unknown classificaiton and df_train_test will be the transactions with known classification which will be used for training and testing the model
+    return df_predict, df_train_test
+
+def extract_features_and_groups(df_train_test):
+    """
+    Extracts the feature columns into X and the group column into y. Returns X and y
+    :param df_train_test: The DataFrame to extract the features and groups from
+    :return: X and y
+    """
 
     # Extract the feature columns into X
     X = df_train_test.iloc[:, 2:]
-    print_dataframe(X, "Features for Machine Learning")
-    print_divider()
 
     # Extract the group column into y
     y = df_train_test["Group"]
-    print_dataframe(y, "Groups for Machine Learning")
-    print_divider()
+
+    return X, y
+
+def train_test_split(X, y):
+    """
+    Splits the data into training and testing sets and returns the split data sets
+    :param X: The features
+    :param y: The groups
+    :return: X_train, X_test, y_train, y_test
+    """
+
+    # Initialize the user_test_size variable
+    user_test_size = 0.3
+
+    try:
+        # Ask the user to enter the percentage of the data to be used for testing
+        user_test_size = float(input("Enter the percentage of the data to be used for testing (0.0 - 1.0): "))
+    except ValueError:
+        print("You did not enter a valid number. Defaulting to 0.3 for a 70/30 split")
 
     # Split the data into training and testing sets
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.30)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=user_test_size)
 
-    print("Split the data into traning and testing sets")
+    return X_train, X_test, y_train, y_test
 
-    # Creating the Random Forest Classifier
+def create_random_forest_classifier():
+    """
+    Creates a Random Forest Classifier and returns it
+    :return: A Random Forest Classifier
+    """
+
+    # Create the Random Forest Classifier
     random_forest_classifier = RandomForestClassifier(n_estimators=100, verbose=3)
 
-    print("Created the Random Forest Classifier")
+    return random_forest_classifier
+
+def train_and_test_model(random_forest_classifier, X_train, y_train, X_test):
+    """
+    Trains the model using the training sets and predicts the response for the test dataset using the trained model and returns the predicted response.
+    :param random_forest_classifier: The Random Forest Classifier
+    :param X_train: The training features
+    :param y_train: The training groups
+    :param X_test: The testing features
+    :return: The predicted response for the test dataset
+    """
 
     print("Starting to train the model...")
 
@@ -173,36 +259,143 @@ def machine_learning(df):
 
     print("Model has been trained")
 
+    print("Predicting the response for the test dataset")
+
     # Predict the response for the test dataset
     y_pred = random_forest_classifier.predict(X_test)
 
-    print("Predicted the response for the test dataset")
+    return y_pred
 
-    # Print the accuracy of the model
-    print(f"Accuracy: {random_forest_classifier.score(X_test, y_test)}")
+def save_model(model, save_path):
+    """
+    Saves the model to the specified path
+    :param model: The model to save
+    :param save_path: The path to save the model to
+    :return: None
+    """
 
-    # Saving the model
-    print(f"Saving the model to {MODEL_SAVE_PATH_LINUX}")
-    joblib.dump(random_forest_classifier, MODEL_SAVE_PATH_LINUX)
+    # Save the model to the specified path
+    print(f"Saving the model to {save_path}")
+    joblib.dump(model, save_path)
+
+def predict_all_unknown(model, df_predict):
+    """
+    Predicts the group for all unknown transactions and returns the DataFrame with the predicted groups
+    :param model: The trained model
+    :param df_predict: The DataFrame with the unknown transactions
+    :return: The DataFrame with the predicted groups
+    """
 
     # Fit the trained model to the prediction data
-    y_predict = random_forest_classifier.predict(df_predict.iloc[:, 2:])
+    y_predict = model.predict(df_predict.iloc[:, 2:])
 
     # Change the 3's to the predicted values
     df_predict['Group'] = y_predict
 
     # Print the DataFrame
     print_dataframe(df_predict, "Predicted Data")
-    print_divider()
+
+    return df_predict
+
+def machine_learning(df):
+    """
+    Performs machine learning on the DataFrame and saves the model to the specified path. Calls the necessary functions to perform machine learning.
+    :param df: The DataFrame to perform machine learning on
+    :return: None
+    """
+
+    df_predict, df_train_test = separate_unknown(df)
+
+    # Print the DataFrames
+    print_dataframe(df_predict, "Predict Data")
+    print_dataframe(df_train_test, "Train and Test Data")
+
+    # Extract the features and groups
+    X, y = extract_features_and_groups(df_train_test)
+
+    # Print the X(Features) and y(Groups) data
+    print_dataframe(X, "Features for Machine Learning")
+    print_dataframe(y, "Groups for Machine Learning")
+
+    print("Splitting the data into traning and testing sets")
+
+    # Split the data into training and testing sets
+    X_train, X_test, y_train, y_test = train_test_split(X, y)
 
 
-def main():
+    print("Creating the Random Forest Classifier")
+    random_forest_classifier = create_random_forest_classifier()
+
+    # Train and test the model
+    y_pred = train_and_test_model(random_forest_classifier, X_train, y_train, X_test)
+
+    # Print the y_pred data
+    print_dataframe(y_pred, "Predicted Data for Machine Learning")
+
+    # Print the accuracy of the model
+    print(f"Accuracy: {random_forest_classifier.score(X_test, y_test)}")
+
+    # Saving the model
+    save_model(random_forest_classifier, MODEL_SAVE_PATH_LINUX)
+
+    # Predict all unknown transactions
+    df_predict = predict_all_unknown(random_forest_classifier, df_predict)
+
+def user_print_input(data_final_df, features_df, combined_df):
+    """
+    Asks the user if they want to print the dataframes and prints them if the user enters 'y' or 'Y' and does nothing if the user enters 'n' or 'N' or anything else.
+    :param data_final_df: The data_final DataFrame
+    :param features_df: The features DataFrame
+    :param combined_df: The combined DataFrame
+    :return: None
+    """
+
+    print_data = input("Do you want to print the dataframes? (y/n): ")
+
+    if print_data.lower() == 'y':
+        print_dataframe(data_final_df, "Data Final")
+        print_dataframe(features_df, "Features")
+        print_dataframe(combined_df, "Combined Data")
+    else:
+        print("Dataframes will not be printed")
+
+def start_ml(combined_df):
+    """
+    Asks the user if they want to start machine learning and starts machine learning if the user enters 'y' or 'Y' and does nothing if the user enters 'n' or 'N' or anything else.
+    :param combined_df: The combined DataFrame
+    :return: None
+    """
+
+    # Reference the global variable to modify it
     global MODEL_SAVE_PATH_LINUX
 
-    # Ask the user for the file name for the model
-    file_name = input("Enter the file name for the final model: ")
+    start_ml = input("Do you want to start machine learning? (y/n): ")
 
-    MODEL_SAVE_PATH_LINUX = MODEL_SAVE_PATH_LINUX + file_name + MODEL_EXTENSION
+    if start_ml.lower() == 'n':
+        print("Machine Learning will not start")
+        exit(0)
+    elif start_ml.lower() == 'y':
+        print("Machine Learning will start")
+
+        # Ask the user to enter the file name for the model to be saved
+        file_name = input("Enter the file name for the final model: ")
+
+        # Set the model save path
+        MODEL_SAVE_PATH_LINUX = MODEL_SAVE_PATH_LINUX + file_name + MODEL_EXTENSION
+
+        print(f"Model will be saved to {MODEL_SAVE_PATH_LINUX}")
+
+        # Start machine learning here
+        machine_learning(combined_df)
+    else:
+        print("Invalid input. Machine Learning will not start")
+        exit(0)
+
+def main():
+    """
+    The main function to run the program. Calls the necessary functions to perform the program of creating the DataFrames, merging them, and starting machine learning.
+    :return: None
+    """
 
     # Initialize the data_final DataFrame
     data_final_df = create_dataframe(LINUX_PATH_1, False)
@@ -213,9 +406,13 @@ def main():
     # Merge the DataFrames
     combined_df = merge_dataframes(data_final_df, features_df)
 
-    # Start machine learning here
-    machine_learning(combined_df)
+    # User input to ask if user wants to print the dataframes
+    user_print_input()
+
+    # User input to ask if the user wants to start machine learning
+    start_ml(combined_df)
 
 
 if __name__ == '__main__':
+    # Call the main function
     main()
